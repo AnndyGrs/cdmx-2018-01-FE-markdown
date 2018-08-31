df5e74a85be6c07bcd7e12843a8ccd09552104f5
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-31 13:10:19 -0500

    Muestra el texto del enlace

--- mdLinks/markdown-links.js
+++ mdLinks/markdown-links.js
@@ -13,12 +13,24 @@ const readFile = () => {
 readFile(callBack = data => getUrl(data));
 
 const getUrl = (data) => {
-  let rgEx = /(http:\/\/|https:\/\/|www\.)[^\s]+/gim;
-  let arrLinks = data.match(rgEx);
+  let rgExLink = /(http:\/\/|https:\/\/|www\.)[^\s]+/gim;
+  let rgExText = /\[[A-Z_ \/$]*\]/gim;
+  let arrText = data.match(rgExText);
+  let arrLinks = data.match(rgExLink);
   let links = [];
+  let text = [];
   let i = 0;
   for (i; i < arrLinks.length; i++) {
     links[i] = arrLinks[i].slice(0, -1);
   }
-  console.log(links); 
+  let j = 0;
+  for (j; j < arrText.length; j++) {
+    text[j] = arrText[j].slice(1, -1);
+  }
+  let k = 0;
+  let info = [];
+  for (k; k < text.length; k++) {
+    info.push(text[k] + ' => ' + links[k]);
+  }
+  console.log(info);  
 };
