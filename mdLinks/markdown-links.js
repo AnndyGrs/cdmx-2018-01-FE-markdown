1c1209f6bb820627b84ef39c43634eafd0bdfd78
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-09-04 15:18:47 -0500

    Se agrega la validacion del status de los links

--- mdLinks/markdown-links.js
+++ mdLinks/markdown-links.js
@@ -1,36 +1,49 @@
 const fs = require('fs');
+const fetch = require('node-fetch');
+// const path = require('path');
 
-const readFile = () => {
+// const validatePath = () => {
+//   let pathFile = path.isAbsolute();
+//   if (pathFile === true) {
+//     return pathFile;
+//   } else {
+//     pathFile = path.resolve(pathFile);
+//   }
+//   return pathFile;
+// };
+
+const readFile = (errCallback, callBack) => {
   fs.readFile('../README.md', 'utf8', (err, data) => {
     if (err) {
-      console.log(err);
+      errCallback(err);
     } else {
-      callBack(data);            
+      callBack(data);
     }
   });
 };
 
-readFile(callBack = data => getUrl(data));
+readFile((err) => console.log(err), data => getUrl(data));
 
 const getUrl = (data) => {
-  let rgExLink = /(http:\/\/|https:\/\/|www\.)[^\s]+/gim;
-  let rgExText = /\[[A-Z_ \/$]*\]/gim;
-  let arrText = data.match(rgExText);
-  let arrLinks = data.match(rgExLink);
-  let links = [];
-  let text = [];
-  let i = 0;
-  for (i; i < arrLinks.length; i++) {
-    links[i] = arrLinks[i].slice(0, -1);
+  const rgExLink = /(http:\/\/|https:\/\/|www\.)[^\s]+/gim;
+  const rgExText = /\[[a-zA-Z_ -\/$]*\]/gim;
+  const arrText = data.match(rgExText);
+  const arrLinks = data.match(rgExLink);
+  for (let i = 0; i < arrText.length; i++) {
+    arrText[i] = arrText[i].slice(1, -1);
   }
-  let j = 0;
-  for (j; j < arrText.length; j++) {
-    text[j] = arrText[j].slice(1, -1);
+  for (let i = 0; i < arrLinks.length; i++) {
+    arrLinks[i] = arrLinks[i].slice(0, -1);
+    fetch(arrLinks[i]).then((response) => {
+      let status = response.status;
+      if (status === 200) {
+        console.log('Texto: ' + arrText[i] + ', Link: ' + arrLinks[i] + ', Status: ' + status + ' OK');
+      } else {
+        console.log('Texto: ' + arrText[i] + ', Link: ' + arrLinks[i] + ', Status: ' + status + ' FAIL');
+      }
+    })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
-  let k = 0;
-  let info = [];
-  for (k; k < text.length; k++) {
-    info.push(text[k] + ' => ' + links[k]);
-  }
-  console.log(info);  
 };
