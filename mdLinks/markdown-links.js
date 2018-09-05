c17d52c80cbd2214c08f6bcf64ec7886c044d0c7
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-09-05 18:26:21 -0500

    Se organiza la informacion dentro de un arreglo de objetos

--- mdLinks/markdown-links.js
+++ mdLinks/markdown-links.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
-// const path = require('path');
+const path = require('path');
 
-// const validatePath = () => {
-//   let pathFile = path.isAbsolute();
-//   if (pathFile === true) {
-//     return pathFile;
-//   } else {
-//     pathFile = path.resolve(pathFile);
-//   }
-//   return pathFile;
-// };
+const route = '../README.md';
+
+const validatePath = (route) => {
+  if (route === undefined) {
+    console.log('No es una ruta valida');
+  } else {
+    let pathFile = path.resolve(route);
+    return pathFile;
+  }
+};
 
 const readFile = (errCallback, callBack) => {
-  fs.readFile('../README.md', 'utf8', (err, data) => {
+  fs.readFile(route, 'utf8', (err, data) => {
     if (err) {
       errCallback(err);
     } else {
@@ -26,24 +27,45 @@ readFile((err) => console.log(err), data => getUrl(data));
 
 const getUrl = (data) => {
   const rgExLink = /(http:\/\/|https:\/\/|www\.)[^\s]+/gim;
-  const rgExText = /\[[a-zA-Z_ -\/$]*\]/gim;
+  const rgExText = /\[[\¿A-Za-záéíóúÁÉÍÓÚ_ \.\?\(\)\\\/\-$]*\]\(/gim;
   const arrText = data.match(rgExText);
   const arrLinks = data.match(rgExLink);
   for (let i = 0; i < arrText.length; i++) {
-    arrText[i] = arrText[i].slice(1, -1);
+    arrText[i] = arrText[i].slice(1, -2);
   }
   for (let i = 0; i < arrLinks.length; i++) {
     arrLinks[i] = arrLinks[i].slice(0, -1);
-    fetch(arrLinks[i]).then((response) => {
-      let status = response.status;
-      if (status === 200) {
-        console.log('Texto: ' + arrText[i] + ', Link: ' + arrLinks[i] + ', Status: ' + status + ' OK');
-      } else {
-        console.log('Texto: ' + arrText[i] + ', Link: ' + arrLinks[i] + ', Status: ' + status + ' FAIL');
-      }
-    })
-      .catch((err) => {
-        console.log(err.message);
-      });
   }
+  let info = [];
+  for (let i = 0; i < arrLinks.length; i++) {
+    info.push({
+      href: arrLinks[i],
+      text: arrText[i],
+      file: validatePath(route)
+    });
+  }
+  validateURL(info);
+};
+
+const validateURL = (info) => {
+  let urlStatus = info.map((obj) => {
+    return {status: '', ...obj}
+  });
+  urlStatus.forEach(links => {
+    fetch(links.href).then((response) => {
+      if (response.status === 404) {
+        links.status = `${response.status} FAIL`;
+      } else {
+        links.status = `${response.status} OK`;
+      }
+      console.log(urlStatus);
+      // urlStats(urlStatus);
+    });
+  });
+};
+
+const urlStats = (urlStatus) => {
+  let total = [];
+  let unique = [];
+
 };
